fc93f0bb8112952275bcfdb5ead082664844ce9f
jovi <huangf@garena.com>
2016-03-05 18:38:15 +0800

    add new test case in spec file

--- backend/src/resources/v1/mediaDocs/mediaDoc.spec.js
+++ backend/src/resources/v1/mediaDocs/mediaDoc.spec.js
@@ -1,20 +1,26 @@
 'use strict';
 
-
 const app = require('../../../server');
 const request = require('supertest').agent(app.listen());
 
 const expect = require('chai').expect;
 const should = require('should');
 
-
-describe('GET /mediaDoc', function() {
+describe('GET /api/v1/mediaDocs', function() {
     it('should respond with 200 type Array', function(done) {
         request
             .get('/api/v1/mediaDocs')
             .expect(200, function(err, res) {
-                expect(Array.isArray(res.body.payload)).to.be.true;
+                expect(Array.isArray(res.body.payload.MediaDoc)).to.be.true;
                 done();
             });
+    });
+});
+
+describe('Delete /api/v1/mediaDocs/', function() {
+    it('should forbidden without authorised role', function(done) {
+        request
+            .delete('/api/v1/mediaDocs/any-resource-name')
+            .expect(401, done);
     });
 });
--- backend/src/resources/v1/root/root.spec.js
+++ backend/src/resources/v1/root/root.spec.js
@@ -1,17 +1,15 @@
 'use strict';
 
-
-const app = require('../../server');
+const app = require('../../../server');
 const request = require('supertest').agent(app.listen());
 
 const expect = require('chai').expect;
 const should = require('should');
 
-
-describe('GET /', function() {
-    it('should respond with 403', function(done) {
+describe('GET /api/v1', function() {
+    it('should respond with 200', function(done) {
         request
-            .get('/')
+            .get('/api/v1')
             .expect(403, done);
     });
 });
