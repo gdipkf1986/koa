aa8e8b7a5a092be2e224db2f804fd3994fc2336a
jovi <huangf@garena.com>
2016-03-11 12:16:53 +0800

    add development configuration

--- backend/src/config/environment/development.js
+++ backend/src/config/environment/development.js
@@ -4,5 +4,30 @@
 
 // Development configuration
 module.exports = {
-  
+    logType: 'tiny',
+    db: {
+        host: 'localhost',
+        port: '',
+        database: '',
+        username: 'root',
+        password: null,
+        'dialect': 'mysql'
+    },
+    s3: {
+        secretKey: '',
+        bucket: '',
+        accessKeyId: ''
+    },
+    grant: {
+        server: {
+            protocol: 'http',
+            host: '127.0.0.1:9000'
+        },
+        google: {
+            key: '',
+            secret: '',
+            callback: '/oauth/callback',
+            'scope': 'https://www.googleapis.com/auth/plus.login'
+        }
+    }
 };
