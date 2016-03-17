b207612b186335b39d642277505d3d89b81f1b3f
jovi <huangf@garena.com>
2016-03-17 16:08:08 +0800

    move vistior as independence visitor

--- frontend/babelTransformers/transformer.js
+++ frontend/babelTransformers/transformer.js
@@ -12,7 +12,44 @@ const CWD_PREFIX = '~';
 
 import features from './scripts/features';
 
-export default function ({types:t}) {
+
+const Visitor_if_statement = {
+    IfStatement: {
+        enter(nodePath, pluginPass) {
+            const node = nodePath.node;
+            let feature = null;
+            let isFeatureStatement = false;
+            let featureOnStatements = null;
+            let featureOffStatements = null;
+
+            const test = node.test;
+            if (test.type === 'CallExpression' && test.callee.name === 'features') {
+                isFeatureStatement = true;
+                feature = test.arguments[0].value;
+                featureOffStatements = node.alternate && node.alternate.body;
+                featureOnStatements = node.consequent && node.consequent.body;
+                console.log('call')
+            } else if (test.type === 'UnaryExpression' && test.argument.type === 'CallExpression' && test.argument.callee.name === 'features') {
+                isFeatureStatement = true;
+                feature = test.argument.arguments[0].value;
+                featureOnStatements = node.alternate && node.alternate.body;
+                featureOffStatements = node.consequent && node.consequent.body;
+                console.log('unary')
+            }
+            if (isFeatureStatement) {
+                console.log(`features(${feature}) ====> ${features(feature) + ''}`);
+                if (features(feature) && featureOnStatements) {
+                    nodePath.replaceWithMultiple(featureOnStatements);
+                } else if (!features(feature) && featureOffStatements) {
+                    nodePath.replaceWithMultiple(featureOffStatements);
+                }
+            }
+
+        }
+    }
+};
+
+export default function({types:t}) {
     const cwd = process.cwd();
     return {
         visitor: {
@@ -37,39 +74,8 @@ export default function ({types:t}) {
                 }
 
             },
-            IfStatement: {
-                enter(nodePath, pluginPass) {
-                    const node = nodePath.node;
-                    let feature = null;
-                    let isFeatureStatement = false;
-                    let featureOnStatements = null;
-                    let featureOffStatements = null;
-
-                    const test = node.test;
-                    if (test.type === 'CallExpression' && test.callee.name === 'features') {
-                        isFeatureStatement = true;
-                        feature = test.arguments[0].value;
-                        featureOffStatements = node.alternate && node.alternate.body;
-                        featureOnStatements = node.consequent && node.consequent.body;
-                        console.log('call')
-                    } else if (test.type === 'UnaryExpression' && test.argument.type === 'CallExpression' && test.argument.callee.name === 'features') {
-                        isFeatureStatement = true;
-                        feature = test.argument.arguments[0].value;
-                        featureOnStatements = node.alternate && node.alternate.body;
-                        featureOffStatements = node.consequent && node.consequent.body;
-                        console.log('unary')
-                    }
-                    if (isFeatureStatement) {
-                        console.log(`features(${feature}) ====> ${features(feature) + ''}`);
-                        if (features(feature) && featureOnStatements) {
-                            nodePath.replaceWithMultiple(featureOnStatements);
-                        } else if (!features(feature) && featureOffStatements) {
-                            nodePath.replaceWithMultiple(featureOffStatements);
-                        }
-                    }
-
-                }
-
+            IfStatement(nodePath, pluginPass){
+                nodePath.traverse(Visitor_if_statement)
             }
         }
     }
